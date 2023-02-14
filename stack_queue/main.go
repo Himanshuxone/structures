8351f133e59d57fb2e01649e07e02c8c6f3100b6
Himanshu <himanshusdec8@gmail.com>
2023-02-14 23:18:15 +0530

    stack_queue

---
+++ stack_queue/main.go
@@ -0,0 +1,57 @@
+package main
+
+import "fmt"
+
+type stack []int
+
+func main() {
+	var stacks stack = make([]int, 0)
+	data := []int{1, 2, 3, 4, 5}
+	for i := len(data) - 1; i >= 0; i-- {
+		stacks.Push(data[i])
+	}
+	fmt.Printf("Stacks => %+v\n", stacks)
+	for _ = range stacks {
+		stacks.Pop()
+	}
+	fmt.Printf("Stacks => %+v\n", stacks)
+	for i := len(data) - 1; i >= 0; i-- {
+		stacks.Enqueue(data[i])
+	}
+	fmt.Printf("Stacks => %+v\n", stacks)
+	stacks.Dequeue()
+	fmt.Printf("Stacks => %+v\n", stacks)
+}
+
+func (s *stack) Push(item int) {
+	*s = append(*s, item)
+}
+
+func (s *stack) IsEmpty() bool {
+	return len(*s) == 0
+}
+
+func (s *stack) Pop() {
+	if len(*s) == 0 {
+		s.IsEmpty()
+	} else {
+		index := len(*s) - 1
+		pop := (*s)[index]
+		fmt.Printf("%d popped \n", pop)
+		*s = (*s)[:index]
+	}
+}
+
+func (s *stack) Dequeue() {
+	if len(*s) == 0 {
+		s.IsEmpty()
+	} else {
+		item := (*s)[0]
+		fmt.Println("Dequeue First Element", item)
+		*s = (*s)[1:len(*s)]
+	}
+}
+
+func (s *stack) Enqueue(item int) {
+	*s = append(*s, item)
+}
